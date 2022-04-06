2c02b220bd0a4bd0eba3893c6018cd67ea522190
Ben Drucker <bvdrucker@gmail.com>
2022-04-05 17:04:09 -0700

    Automatically populate configuration aliases before `terraform validate` (#3)

---
+++ test/configuration-alias/versions.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_version = ">= 1, < 2"
+
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 1"
+
+      configuration_aliases = [null.foo]
+    }
+  }
+}
